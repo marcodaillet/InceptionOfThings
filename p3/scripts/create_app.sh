35da5b6beca998b0269995ab27f6430f035f506e
Marc-Eloi Daillet <55747965+marcodaillet@users.noreply.github.com>
2023-07-31 17:18:43 +0200

    Update create_app.sh

--- p3/scripts/create_app.sh
+++ p3/scripts/create_app.sh
@@ -14,16 +14,3 @@ argocd app create mdaillet-app \
 
 # Deploy the app
 argocd app sync mdaillet-app
-
-# Port forwarding to access the application locally
-kubectl port-forward svc/my-app -n dev 8888:80 &
-port_forward_pid=$!
-
-# Wait for a brief moment to allow port forwarding to be established
-sleep 5
-
-# Display a message indicating successful setup
-echo "The application is now accessible via http://localhost:8888/"
-
-# Keep the port forwarding command running in the background
-wait $port_forward_pid
